4da8b9c265d0303f57553fffcb91d662487b5865
Allen Lee <allen870619@gmail.com>
2023-07-08 22:58:09 +0800

    Add private chatgpt channel

--- Message.py
+++ Message.py
@@ -548,7 +548,7 @@ async def messageReact(self, client, ctx, isFromEdit=False):
         await ctx.channel.send(str)
     
     # openai chat
-    elif ctx.channel.id == 1081213613274050620 and len(origin) > 0:
+    elif (ctx.channel.id == 1081213613274050620 or ctx.channel.id == 1127251556690034738) and len(origin) > 0:
         resp = openai_gpt_chat(origin, user_id=ctx.author.id)
         await ctx.channel.send(resp)
 
