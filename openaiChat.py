e2d5e1881dce58668a067cd20aa0fd3217430c75
Allen Lee <allen870619@gmail.com>
2023-10-17 10:44:57 +0800

    Fall back chatGPT to `gpt-3.5-turbo`

--- openaiChat.py
+++ openaiChat.py
@@ -32,7 +32,7 @@ def openai_gpt_chat(msgToSend: str, user_id):
     tmp_list.append(to_send_msg)
     
     try: 
-        response = openai.ChatCompletion.create(model="gpt-4",
+        response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
                                                 messages=tmp_list,
                                                 max_tokens=max_token,
                                                 user=f"{user_id}")
