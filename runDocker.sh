f7fa388e545918e40836ff2a456b0167f6224a97
Allen Lee <allen870619@gmail.com>
2024-02-18 11:44:51 +0800

    Adjust run script

--- runDocker.sh
+++
@@ -1,8 +0,0 @@
-# get log and save
-# docker log --tail 500 >> "chat_$(date +"%Y-%m-%dT%H:%M:%S").log"
-
-# build all
-docker stop gura
-docker rm gura
-docker create --name gura allen870619/gura_discord_bot
-docker start gura
